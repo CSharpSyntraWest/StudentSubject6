ffdbf7e9c890b583d569560de2846571c7c938c5
CsharpSyntraWestKortrijk <61596529+CSharpSyntraWest@users.noreply.github.com>
2021-02-11 16:46:45 +0100

    Add files via upload

---
+++ StudentSubjects/Controllers/CoursesController.cs
@@ -0,0 +1,23 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentSubjects.Controllers
+{
+    [Route("api/courses")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        private readonly IRepositoryManager _repository;
+        public CoursesController(IRepositoryManager repository)
+        {
+            _repository = repository;
+        }
+        //[HttpGet]
+        //public IAction
+    }
+}
